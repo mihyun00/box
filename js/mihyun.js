a3741a863f0ef1e0582a95395f634b482b055e35
mihyun00 <k8768352@naver.com>
2020-03-17 14:31:04 +0900

    미현
    css, js 버튼 클릭시 해당연도 슬라이드 표출 수정 중

--- js/mihyun.js
+++ js/mihyun.js
@@ -4,10 +4,12 @@ $(".sect3-slide").mCustomScrollbar({
     theme: "dark-thin"
   })
   //sect3 buootn
-  $(".year-button").click(function(){
-      //button 부분 . 클릭하는 버튼만 배경, 글자색 변경
-      $(".year-button").removeClass('on')
-      $(this).addClass('on')
-      //slide 부분 . 클릭하는 연도의 slide 표출
+//   $(".year-button").click(function(){
+//       //button 부분 . 클릭하는 버튼만 배경, 글자색 변경
+//       $(".year-button").removeClass('on')
+//       $(this).addClass('on')
+//       //slide 부분 . 클릭하는 연도의 slide 표출
   
-  })
+//   })
+
+$()
