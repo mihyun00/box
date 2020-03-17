152401f6c57ab788a058d9a04abf319b0e373a4d
mihyun00 <k8768352@naver.com>
2020-03-17 14:23:38 +0900

    파일 정리
    각 css, js 나눠서 link재정비

---
+++ js/juyeon.js
@@ -0,0 +1,12 @@
+
+      var swiper = new Swiper(".swiper-container", {
+        spaceBetween: 30,
+        autoplay: {
+          delay: 3000
+        },
+        effect: "fade",
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        }
+      })
