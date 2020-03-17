6d36aed5d5147618c03fb00620a0caeb6244ef23
mihyun00 <k8768352@naver.com>
2020-03-17 17:54:42 +0900

    0317
    sect3번 수정
    sect4번 수정
    스와이퍼 문제로 지현, 주연님 js 파일 삭제

--- js/mihyun.js
+++ js/mihyun.js
@@ -1,17 +1,30 @@
-//sect3 커스텀 스크롤 
+//sect3 커스텀 스크롤
 $(".sect3-slide").mCustomScrollbar({
   axis: "x", // vertical and horizontal scrollbar
   theme: "dark-thin"
 })
 
 //sect3 buootn 클릭하면 연도별 슬라이드 표출되며 버튼 색 변경
-$('.year-btn button').click(function(){
-  $('.year-btn button').removeClass('on')
-  $(this).addClass('on')
+$(".year-btn button").click(function() {
+  $(".year-btn button").removeClass("on")
+  $(this).addClass("on")
 
   var Ye = $(this).index()
-  $('.sect3-slide ul').hide()
-  $('.sect3-slide ul').eq(Ye).show()
+  $(".sect3-slide ul").hide()
+  $(".sect3-slide ul")
+    .eq(Ye)
+    .show()
 })
-
-
+//sect4 swiper
+var swiper = new Swiper(".sect4-swiper-container", {
+  spaceBetween: 10,
+  slidesPerView: 8,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+})
