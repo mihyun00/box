18f2ae694c6f9ec957c7a960c46bbacc53530c4c
mihyun00 <k8768352@naver.com>
2020-03-18 18:45:23 +0900

    0318+
    css 경로 변경
    js 스와이퍼 주석 추가

--- js/mihyun.js
+++ js/mihyun.js
@@ -84,3 +84,18 @@ var swiper = new Swiper('.sect3-swiper-container', {
   //   disableOnInteraction: false
   // }
 });
+
+//스와이퍼 반응형 소스코드
+// breakpoints: {
+//   640: {
+//     slidesPerView: 2,
+//     spaceBetween: 20,
+//   },
+//   768: {
+//     slidesPerView: 4,
+//     spaceBetween: 40,
+//   },
+//   1024: {
+//     slidesPerView: 5,
+//     spaceBetween: 50,
+//   },
