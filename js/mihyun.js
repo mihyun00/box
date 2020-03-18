5a37f76a719875022a3f340d13f62327d43705d1
mihyun00 <k8768352@naver.com>
2020-03-18 18:04:38 +0900

    0318
    sect3번 반응형 작성 중
    sect3번 스크립트 수정
    sect4번 메뉴따라 이동하는 a tag 스크립트 수정

--- js/mihyun.js
+++ js/mihyun.js
@@ -5,6 +5,7 @@ $(".sect3-slide").mCustomScrollbar({
 })
 
 //sect3 buootn 클릭하면 연도별 슬라이드 표출되며 버튼 색 변경
+$(".2000year").hide()
 $(".year-btn button").click(function() {
   $(".year-btn button").removeClass("on")
   $(this).addClass("on")
@@ -15,8 +16,9 @@ $(".year-btn button").click(function() {
     .eq(Ye)
     .show()
 })
+
 //sect4 swiper
-var swiper = new Swiper(".sect4-swiper-container", {
+var swiper_sect4 = new Swiper(".sect4-swiper-container", {
   spaceBetween: 10,
   slidesPerView: 8,
   spaceBetween: 30,
@@ -25,6 +27,60 @@ var swiper = new Swiper(".sect4-swiper-container", {
   loopFillGroupWithBlank: true,
   autoplay: {
     delay: 3500,
-    disableOnInteraction: false,
-  },
+    disableOnInteraction: false
+  }
 })
+
+//a 태그 사용자 선택에 따라 위치 이동
+var b = $(".line-a").offset().left
+
+pointFuc(0)
+$(".book-navtxt li").click(function() {
+  var idx = $(this).index()
+  pointFuc(idx)
+})
+
+function pointFuc(i) {
+  var p = $(".book-navtxt li")
+    .eq(i)
+    .offset().left //각 li의 위치 값
+  var h =
+    $(".book-navtxt li")
+      .eq(i)
+      .width() / 2 //li의 width/2
+  $(".line-a").animate({ left: p - b + h - 3 })
+}
+$(".book-navtxt li").click(function(e) {
+  e.preventDefault()
+})
+
+//book-navtxt 메뉴 선택시 해당 분야 도서가 첫번째로 이동
+
+//다른 분야로 swiper가 넘어갈 때 book-nevtxt, a 가 함께 이동 
+
+
+
+
+// swiper_sect4.on("slideChange", function() {
+//   var activeIdx = swiper.activeIndex
+//   console.log(activeIdx)
+ 
+// })
+
+//sect3 모바일 버전 swiper
+var swiper = new Swiper('.sect3-swiper-container', {
+  effect: 'cube',
+  grabCursor: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 40,
+    shadowScale: 1,
+  },
+  direction: 
+    'vertical',
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false
+  // }
+});
