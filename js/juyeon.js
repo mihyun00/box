18b0918f25a913c872df996fac66af06a6d4c8e2
parkhyewon <ettp77@naver.com>
2020-03-19 17:36:19 +0900

    0319 주연
    section2 수정중.............

--- js/juyeon.js
+++ js/juyeon.js
@@ -12,10 +12,30 @@ var swiper = new Swiper(".swiper-container", {
 })
 
 $(document).scroll(function() {
+
   var scrl = $(this).scrollTop()
+
+  
+
   if (scrl > 0) {
     $("header").addClass("on")
   } else if (scrl == 0) {
     $("header").removeClass("on")
   }
+
+
+  if(scrl < 350) {
+    $('.sect2_logo img').animate({'opacity':'1','top':'40%'})
+    $('.sect2_txt p').delay(200).animate({'opacity':'1','top':'40%'})
+    $('.sect2 button').delay(400).animate({'opacity':'1','top':'40%'})
+  }
+  else if(scrl < 350){
+    $('.sect2_logo img').animate({'opacity':'0','top':'70%'})
+    $('.sect2_txt p').animate({'opacity':'0','top':'70%'})
+    $('.sect2 button').delay(400).animate({'opacity':'0','top':'70%'})
+  }    
 })
+  
+
+
+
