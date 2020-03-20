65af8685990e9969bdb22b3dac9b6a3593b2bfdb
parkhyewon <ettp77@naver.com>
2020-03-20 17:35:41 +0900

    0320 1차수정

--- js/juyeon.js
+++ js/juyeon.js
@@ -12,3 +12,14 @@ var swiper = new Swiper(".sect1_swiper-container", {
 })
 
 //연혁과 어바웃 창비 합침으로 스크롤 이벤트 js 미현js로 이동
+$(document).scroll(function() {
+  var scrl = $(this).scrollTop()
+
+  if (scrl > 0) {
+    $("header").addClass("on")
+  } else if (scrl == 0) {
+    $("header").removeClass("on")
+  }
+})
+
+$()
