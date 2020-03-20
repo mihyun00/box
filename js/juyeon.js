a7c384539b3a6d7b4d2a002858a9348cb2ec1eca
mihyun00 <k8768352@naver.com>
2020-03-20 17:24:31 +0900

    0320 master
    js 수정

--- js/juyeon.js
+++ js/juyeon.js
@@ -11,4 +11,27 @@ var swiper = new Swiper(".sect1_swiper-container", {
   }
 })
 
-//연혁과 어바웃 창비 합침으로 스크롤 이벤트 js 미현js로 이동
+//연혁과 어바웃 창비 합침으로 스크롤 이벤트 js 미현js로 복사
+// header 부분 이벤트 발생 제외 후 주석처리 
+
+$(document).scroll(function() {
+
+  var scrl = $(this).scrollTop()
+
+  if (scrl > 0) {
+    $("header").addClass("on")
+  } else if (scrl == 0) {
+    $("header").removeClass("on")
+  }
+
+  // if(scrl < 350) {
+  //   $('.sect4_logo img').animate({'opacity':'1','top':'40%'})
+  //   $('.sect4_txt p').delay(200).animate({'opacity':'1','top':'40%'})
+  //   $('.sect4 button').delay(400).animate({'opacity':'1','top':'40%'})
+  // }
+  // else if(scrl < 350){
+  //   $('.sect4_logo img').animate({'opacity':'0','top':'70%'})
+  //   $('.sect4_txt p').animate({'opacity':'0','top':'70%'})
+  //   $('.sect4 button').delay(400).animate({'opacity':'0','top':'70%'})
+  // }
+})
