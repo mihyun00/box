2c2a325a5121b083201fb7f769fdd5c5b4638d27
mihyun00 <k8768352@naver.com>
2020-03-30 18:28:17 +0900

    0330 mihyun
    sect2번 수정 중

--- js/mihyun.js
+++ js/mihyun.js
@@ -1,11 +1,5 @@
-// $('a').click(function(e) {
-//   e.preventDefault()
-// })
-
 //sect2
-var swiper = new Swiper('.sect2-swiper-container', {
-  slidesPerView: 5,
-  slidesPerGroup: 5,
+var swiper = new Swiper(".sect2-swiper-container", {
   loop: true,
   loopFillGroupWithBlank: true,
   breakpoints: {
@@ -18,26 +12,48 @@ var swiper = new Swiper('.sect2-swiper-container', {
     },
     1024: {
       slidesPerView: 5
+      // slidesPerGroup: 5
     }
   },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+})
+
+var b = $(".sect2-pop").offset().left //포인터의 기본 위치 값
+
+pointFuc(0)
+$(".sect2-swiper-container .swiper-wrapper .swiper-slide").mouseenter(function() {
+  var idx = $(this).index()
+  pointFuc(idx)
+})
+
+function pointFuc(i) {
+  var p = $(".sect2-swiper-container .swiper-wrapper .swiper-slide")
+    .eq(i)
+    .offset().left
+  var h =
+    $(".sect2-swiper-container .swiper-wrapper .swiper-slide")
+      .eq(i)
+      .width() / 2
+  // $(".sect2-pop").animate({ left: p - b + h }, function() {
+  //   $(".sect2-pop").fadeIn()
+  //   $(".sect2-swiper-container .swiper-wrapper .swiper-slide").mouseleave(function() {
+  //     $(".sect2-pop").fadeOut()
+  //   })
+  // })
+}
 
 //sect2 도서 클릭하면 해당되는 내용 표출
-$(".sect2-swiper-container .swiper-slide .bookIMG a").click(function() {
+$(".sect2-swiper-container .swiper-slide .bookIMG a").click(function(e) {
+  e.preventDefault()
   var href = $(this).attr("href")
 
-  $(".sect2-slide-txtBOX-inner ul li").css({'opacity':0})
-  $(href).css({'opacity':1})
+  $(".sect2-slide-txtBOX-inner ul li").css({ opacity: 0 })
+  $(href).css({ opacity: 1 })
 })
-$('.bookIMG a').click(function(e) {
+$(".bookIMG a").click(function(e) {
   e.preventDefault()
 })
 //sect3 buootn 클릭하면 연도별 슬라이드 표출되며 버튼 색 변경
@@ -93,11 +109,6 @@ var swiper = new Swiper(".sect3-02", {
   }
 })
 
-
-
-
-
-
 //스크롤 이벤트
 
 // $(document).scroll(function() {
@@ -121,4 +132,3 @@ var swiper = new Swiper(".sect3-02", {
 //     $('.sect4 button').delay(400).animate({'opacity':'0','top':'70%'})
 //   }
 // })
-
