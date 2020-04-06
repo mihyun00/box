39d916c6008910d80fa0388d95f01d7e37072f0f
mihyun00 <k8768352@naver.com>
2020-04-06 19:04:35 +0900

    0406 mihyun
    sect2 모두 수정
    sect3 고민 중

--- js/mihyun.js
+++ js/mihyun.js
@@ -1,85 +1,102 @@
 //sect2
-var swiper = new Swiper(".sect2-swiper-container", {
+
+var swiper = new Swiper(".new", {
+  slidesPerView: 5,
+  spaceBetween: 10,
   loop: true,
-  loopFillGroupWithBlank: true,
-  breakpoints: {
-    200: {
-      slidesPerView: 2,
-      spaceBetween: 10,
-      slidesPerGroup: 2
-    },
-    768: {
-      slidesPerView: 4,
-      slidesPerGroup: 4
-    },
-    1024: {
-      slidesPerView: 5,
-      slidesPerGroup: 5
-    }
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
   },
   navigation: {
     nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  }
+    prevEl: ".swiper-button-prev",
+  },
 })
 
-// var b = $(".sect2-pop").offset().left //포인터의 기본 위치 값
-
-// pointFuc(0)
-// $(".sect2-swiper-container .swiper-wrapper .swiper-slide").mouseenter(function() {
-//   var idx = $(this).index()
-//   pointFuc(idx)
-// })
-
-// function pointFuc(i) {
-//   var p = $(".sect2-swiper-container .swiper-wrapper .swiper-slide")
-//     .eq(i)
-//     .offset().left
-//   var h =
-//     $(".sect2-swiper-container .swiper-wrapper .swiper-slide")
-//       .eq(i)
-//       .width() / 2
-//   // $(".sect2-pop").animate({ left: p - b + h }, function() {
-//   //   $(".sect2-pop").fadeIn()
-//   //   $(".sect2-swiper-container .swiper-wrapper .swiper-slide").mouseleave(function() {
-//   //     $(".sect2-pop").fadeOut()
-//   //   })
-//   // })
-// }
-
-//book after
-$('.bookIMG a').mouseenter(function(){
-  $('.bookIMG a').removeClass('on')
-  $(this).addClass('on')
-})
-$('.bookIMG a').mouseleave(function(){
-  $('.bookIMG a').removeClass('on')
+var swiper = new Swiper(".best", {
+  slidesPerView: 5,
+  spaceBetween: 10,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 })
 
+var swiper = new Swiper(".chu", {
+  slidesPerView: 5,
+  spaceBetween: 10,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+})
 
-//sect2 도서 클릭하면 해당되는 내용 표출
-$(".sect2-swiper-container .swiper-slide .bookIMG a").click(function(e) {
+var swiper = new Swiper(".prime", {
+  slidesPerView: 5,
+  spaceBetween: 10,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+})
+
+var swiper = new Swiper(".ebook", {
+  slidesPerView: 5,
+  spaceBetween: 10,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+})
+//도서 탭 클릭하면 글씨 클래스 on + 도서 슬라이드 변경
+$(".sect2-tap ul li a").click(function (e) {
+  e.preventDefault()
+  $(".sect2-tap ul li a").removeClass("on")
+  $(this).addClass("on")
+  var href = $(this).attr("href")
+  $(".sect2-bookslide>div").hide().css({opacity:'0'})
+  $(href).show().css({opacity:'1'})
+})
+
+// //sect2 도서 클릭하면 해당되는 내용 표출
+$(".sect2-swiper-container .swiper-slide a").click(function (e) {
   e.preventDefault()
   var href = $(this).attr("href")
+  // alert(href)
 
-  $(".sect2-slide-txtBOX-inner ul li").css({ opacity: 0 })
-  $(href).css({ opacity: 1 })
+  $(".sect2-book-txt ul li").stop().css({ display: "none"}, 500).animate({ marginTop: "16%",opacity:'0'  })
+  $(href).stop().css({ display: "block"}).animate({ marginTop: "14%" ,opacity:'1' })
 })
-$(".bookIMG a").click(function(e) {
-  e.preventDefault()
-})
-
 
 //sect3 buootn 클릭하면 연도별 슬라이드 표출되며 버튼 색 변경
-$(".year-btn button").click(function() {
+$(".year-btn button").click(function () {
   $(".year-btn button").removeClass("on")
   $(this).addClass("on")
 
   var Ye = $(this).index()
   $(".sect3-swiper-container").hide()
-  $(".sect3-swiper-container")
-    .eq(Ye)
-    .show()
+  $(".sect3-swiper-container").eq(Ye).show()
 })
 
 //sect3 swiper
@@ -93,17 +110,17 @@ var swiper = new Swiper(".sect3-01", {
     200: {
       slidesPerView: 4,
       direction: "vertical",
-      spaceBetween: 20
+      spaceBetween: 20,
     },
     760: {
       slidesPerView: 4,
-      direction: "horizontal"
+      direction: "horizontal",
     },
     1024: {
       slidesPerView: 5,
-      direction: "horizontal"
-    }
-  }
+      direction: "horizontal",
+    },
+  },
 })
 
 var swiper = new Swiper(".sect3-02", {
@@ -115,17 +132,17 @@ var swiper = new Swiper(".sect3-02", {
     200: {
       slidesPerView: 4,
       direction: "vertical",
-      spaceBetween: 20
+      spaceBetween: 20,
     },
     760: {
       slidesPerView: 4,
-      direction: "horizontal"
+      direction: "horizontal",
     },
     1024: {
       slidesPerView: 5,
-      direction: "horizontal"
-    }
-  }
+      direction: "horizontal",
+    },
+  },
 })
 
 //스크롤 이벤트
@@ -151,4 +168,3 @@ var swiper = new Swiper(".sect3-02", {
 //     $('.sect4 button').delay(400).animate({'opacity':'0','top':'70%'})
 //   }
 // })
-
